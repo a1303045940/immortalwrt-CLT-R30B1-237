2f06b97856094e3ba9b73b19c310d0a06b9bb4d2
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-10-21 22:36:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -61,7 +61,7 @@ fi
 # 克隆所需第三方包
 clone_package "https://github.com/gdy666/luci-app-lucky.git" "package/luci-app-lucky"
 clone_package "https://github.com/tty228/luci-app-wechatpush.git" "package/luci-app-wechatpush"
-clone_package "https://github.com/rogueme/luci-app-adguardhome.git" "package/luci-app-adguardhome"
+#clone_package "https://github.com/rogueme/luci-app-adguardhome.git" "package/luci-app-adguardhome"
 clone_package "https://github.com/sirpdboy/luci-app-taskplan.git" "package/luci-app-taskplan"
 # 克隆mentohust解决luci-app-airwhu缺失依赖的警告
 clone_package "https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git" "package/mentohust"
