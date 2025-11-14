a4599ba9454a4e4b85cb33cacfd5ee21403ba16d
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-11-14 22:52:23 +0800

    Update filogic.mk

--- filogic.mk
+++ filogic.mk
@@ -1821,6 +1821,19 @@ define Device/ruijie_rg-x60-pro
 endef
 TARGET_DEVICES += ruijie_rg-x60-pro
 
+define Device/bt_r320-emmc
+  DEVICE_VENDOR := BT
+  DEVICE_MODEL := r320 eMMC
+  DEVICE_DTS := mt7981b-bt-r320-emmc
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-usb3 kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware automount coremark blkid blockdev fdisk f2fsck mkf2fs kmod-mmc
+  KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += bt_r320-emmc
+
 define Device/sl_3000-emmc
   DEVICE_VENDOR := SL
   DEVICE_MODEL := 3000 eMMC
