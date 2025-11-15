01e4c190ce9c0248fc0a25c682758e9668120cd8
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-11-15 17:43:31 +0800

    Update filogic.mk

--- filogic.mk
+++ filogic.mk
@@ -1847,6 +1847,19 @@ define Device/sl_3000-emmc
 endef
 TARGET_DEVICES += sl_3000-emmc
 
+define Device/ph_hy3000-emmc
+  DEVICE_VENDOR := PH
+  DEVICE_MODEL := hy3000 eMMC
+  DEVICE_DTS := mt7981b-ph-hy3000-emmc
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware automount coremark blkid blockdev fdisk f2fsck mkf2fs kmod-mmc
+  KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += ph_hy3000-emmc
+
 define Device/tplink_re6000xd
   DEVICE_VENDOR := TP-Link
   DEVICE_MODEL := RE6000XD
